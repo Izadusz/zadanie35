ea6d86e151492778fc4dc0feac192aa137e1189b
Iza Duszyńska <iza.duszynska@wp.pl>
2020-09-04 12:38:51 +0200

    zadanie35

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function checkNumber(n) {
+    
+    if ((n > 100) && (n<200)) {
+        console.log('Liczba znajduje się w przedziale');
+    } else {
+        console.log('Liczba nie znajduje się w przedziale');
+    }
+}
+checkNumber(250);
